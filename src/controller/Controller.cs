2c01b69cf76e46070e1194785f8979f1b3c1caff
Migbau1 <miguelangelbautistasilva2@gmail.com>
2021-12-22 16:14:00 -0500

    add timelapse to output file

--- src/controller/Controller.cs
+++ src/controller/Controller.cs
@@ -20,23 +20,23 @@ namespace Principal
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            string[] temp;
             int count = texto.Split('n').Length - 1;
-
-            temp =
-            new String[]
-            { $"El Numero de Veces que aparace la letra 'n' son: {count} \n" };
 
             Console.WriteLine("El Numero de Veces que aparace la letra 'n' son: {0} \n", count);
 
-
-            writeInFile(temp);
+            writeInFile($"El Numero de Veces que aparace la letra 'n' son: {count} \n");
 
             stopWatch.Stop();
+
             TimeSpan ts = stopWatch.Elapsed;
-            Console.WriteLine("el tiempo de la tarea 1 es: {0:00}:{1:00}:{2:00}.{3:00}",
+
+            string time = String.Format("el tiempo de la tarea 1 es: {0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
+
+            Console.WriteLine(time);
+            writeInFile(time);
+
         }
 
         //sentences Counter
@@ -46,7 +46,6 @@ namespace Principal
             stopWatch.Start();
 
             List<String> sentences = new List<String>();
-            String[] tempMsg;
             int start = 0;
             int position;
 
@@ -65,19 +64,21 @@ namespace Principal
                 }
             } while (position > 0);
 
-            tempMsg =
-            new String[]
-            {$"El numero de oraciones con mas de 15 caracteres son: {sentences.Count()} \n"};
+            Console.WriteLine(
+                $"El numero de oraciones con mas de 15 caracteres son: {sentences.Count()} \n");
 
-            Console.WriteLine($"El numero de oraciones con mas de 15 caracteres son: {sentences.Count()} \n");
-
-            writeInFile(tempMsg);
+            writeInFile($"El numero de oraciones con mas de 15 caracteres son: {sentences.Count()} \n");
 
             stopWatch.Stop();
+
             TimeSpan ts = stopWatch.Elapsed;
-            Console.WriteLine("el tiempo de la tarea 2 es: {0:00}:{1:00}:{2:00}.{3:00}",
+
+            string time = String.Format("el tiempo de la tarea 2 es: {0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
+
+            Console.WriteLine(time);
+            writeInFile(time);
         }
 
         //paragraph Counter
@@ -87,7 +88,6 @@ namespace Principal
             stopWatch.Start();
 
             List<String> sentences = new List<String>();
-            String[] tempMsg;
             int start = 0;
             int position;
 
@@ -106,19 +106,19 @@ namespace Principal
                 }
             } while (position > 0);
 
-            tempMsg =
-            new String[]
-            {$"El numero de parrafos  son: {sentences.Count()} \n"};
-
             Console.WriteLine($"El numero de parrafos son: {sentences.Count()} \n");
-
-            writeInFile(tempMsg);
+            writeInFile($"El numero de parrafos  son: {sentences.Count()} \n");
 
             stopWatch.Stop();
+
             TimeSpan ts = stopWatch.Elapsed;
-            Console.WriteLine("el tiempo de la tarea 3 es: {0:00}:{1:00}:{2:00}.{3:00}",
+
+            string time = String.Format("el tiempo de la tarea 3 es: {0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
+
+            Console.WriteLine(time);
+            writeInFile(time);
         }
 
         //alphanumeric Counter
@@ -127,8 +127,6 @@ namespace Principal
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-
-            String[] tempMsg;
             texto = texto.Trim().Replace('n'.ToString(), String.Empty)
             .Replace('N'.ToString(), String.Empty);
             texto = new String((from c in texto
@@ -137,24 +135,27 @@ namespace Principal
             ).ToArray());
 
 
-            tempMsg =
-           new String[]
-           {$"El numero de caracteres alfanumericos son: {texto.Count()} \n"};
-
             Console.WriteLine($"El numero de caracteres alfanumericos son: {texto.Count()} \n");
-            writeInFile(tempMsg);
+            writeInFile($"El numero de caracteres alfanumericos son: {texto.Count()} \n");
 
             stopWatch.Stop();
+
             TimeSpan ts = stopWatch.Elapsed;
-            Console.WriteLine("el tiempo de la tarea 4 es: {0:00}:{1:00}:{2:00}.{3:00}",
+
+            string time = String.Format("el tiempo de la tarea 4 es: {0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
+
+            Console.WriteLine(time);
+            writeInFile(time);
+
         }
 
         //function to write or create file "resultado.txt"
-        public void writeInFile(string[] messages)
+        public void writeInFile(string message)
         {
             string path = Environment.CurrentDirectory + "/" + _nameFile;
+
 
             lock (_control)
             {
@@ -163,10 +164,7 @@ namespace Principal
                     using (StreamWriter sw = File.CreateText(path))
                     {
                         Thread.Sleep(100);
-                        foreach (var item in messages)
-                        {
-                            sw.WriteLine(item);
-                        }
+                        sw.WriteLine(message);
                     }
                 }
                 else
@@ -174,10 +172,7 @@ namespace Principal
                     using (StreamWriter sw = File.AppendText(path))
                     {
                         Thread.Sleep(100);
-                        foreach (var item in messages)
-                        {
-                            sw.WriteLine(item);
-                        }
+                        sw.WriteLine(message);
                     }
                 }
             }
